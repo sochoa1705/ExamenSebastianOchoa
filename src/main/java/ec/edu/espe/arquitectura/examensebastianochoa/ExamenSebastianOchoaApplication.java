5e78949afa0430b543efa70eb991e10aa860e71e
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-06-14 17:41:17 -0500

    project load to repo

---
+++ src/main/java/ec/edu/espe/arquitectura/examensebastianochoa/ExamenSebastianOchoaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examensebastianochoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSebastianOchoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenSebastianOchoaApplication.class, args);
+    }
+
+}
